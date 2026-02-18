c50e08314e6a26e10a511ddbc747526fc7aeb79c
jade.lhrk <jade.lhrk.git@gmail.com>
2026-02-18 23:22:23 +0700

    start weather bot project

---
+++ src/main/java/com/jade/weatherbot/LineWeatherBotApplication.java
@@ -0,0 +1,13 @@
+package com.jade.weatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineWeatherBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineWeatherBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jade/weatherbot/LineWeatherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jade.weatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineWeatherBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
